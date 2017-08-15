2b0063f87674088a366b0d2baa916c7b148062aa
NpcHitoshi <rafaelhitoshi@hotmail.com>
2017-08-15 09:24:58 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){  
+	$("#cnpj").mask("99.999.999/9999-99");}
+	);
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
