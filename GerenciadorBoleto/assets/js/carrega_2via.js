e9654d5faa987a957dbbddbd44875554962a7b3c
Vinicius S. Pereira <struginskipereira14@hotmail.com>
2017-09-12 11:15:47 -0300

    Correções 2 Via

--- GerenciadorBoleto/assets/js/carrega_2via.js
+++ GerenciadorBoleto/assets/js/carrega_2via.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 			if (this.readyState == 4 && this.status == 200) {
 				myObj = JSON.parse(this.responseText);
 				$("#valor").val(myObj.valor);
+				execmascara();
 				retiraErro($("#valor"));
-				$("#vencimento").val(myObj.dataAtual);
+				$('#vencimento').val(new Date().toDateInputValue());
 				retiraErro($("#vencimento"));
 			}
 		};
@@ -26,4 +27,26 @@ function retiraErro(campo){
 	var erro = $(campo).next();
 	erro.attr("class", "");
 	erro.html("");
+}
+
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+function execmascara(){
+	$("#valor").val(moeda($("#valor").val()));
+}
+
+function moeda(v){ 
+v=v.replace(/\D/g,"") // permite digitar apenas numero 
+v=v.replace(/(\d{1})(\d{17})$/,"$1.$2") // coloca ponto antes dos ultimos digitos 
+v=v.replace(/(\d{1})(\d{13})$/,"$1.$2") // coloca ponto antes dos ultimos 13 digitos 
+v=v.replace(/(\d{1})(\d{10})$/,"$1.$2") // coloca ponto antes dos ultimos 10 digitos 
+v=v.replace(/(\d{1})(\d{8})$/,"$1.$2") // coloca ponto antes dos ultimos 7 digitos 
+v=v.replace(/(\d{1})(\d{5})$/,"$1.$2") // coloca ponto antes dos ultimos 7 digitos 
+v=v.replace(/(\d{1})(\d{1,2})$/,"$1,$2") // coloca virgula antes dos ultimos 2 digitos 
+v = "R$" + v; 
+return v;
 }
